61eb252e0b31e1736447498fce457e077bd79104
Maggie Avery <msavery@berkeley.edu>
2020-09-28 12:02:29 -0700

    Chapter 6

---
+++ _build/jupyter_execute/folder_06/W6_prob_dist.py
@@ -0,0 +1,203 @@
+# 6.1 Probability Distributions
+
+**Outline:**
+* Introduction to probability distributions and empirical distributions
+* Bionomial, Poisson, and Gamma distributions for random events
+
+
+## Additional Assigned Reading
+
+Data 8 textbook "Computational and Inferential Thinking: The Foundations of Data Science" By Ani Adhikari and John DeNero [Chapter 10 Sampling and Empirical Distributions](https://www.inferentialthinking.com/chapters/10/Sampling_and_Empirical_Distributions.html). This should overlap with your assigned reading for Data 8.
+
+## Flipping a coin
+
+Let's pretend we are flipping a coin 10 times using ```np.random.choice([0, 1])```. How many times will be heads? 1 is heads, 0 is tails. Let's use a `for` loop and get Python to simulate such a coin flip scenario for us.
+
+The code will get looped through 10 times -- specified by ```range(0,10)```.
+
+import numpy as np
+import matplotlib.pyplot as plt
+import scipy as scipy
+from scipy import stats
+
+for flip in range(0,10):
+    flip_result = np.random.choice([0, 1])
+    print(flip_result)
+
+Now let's record how many times the result was heads. We will make a list called `flip_results` and have it be blank to start. Each time we go through the code we will append the result to the list:
+
+flip_results = []
+
+for flip in range(0,10):
+    flip_result = np.random.choice([0, 1])
+    flip_results.append(flip_result)
+    
+flip_results    
+
+We can calculate how many times were heads by taking the sum of the list:
+
+np.sum(flip_results)
+
+Now we'll repeat our expirement of 10 flips 10 times keeping a count of the number of heads in each experiment in `number_heads`.
+
+number_heads = []
+
+for flip_experiment in range (0,10):
+
+    flip_results = []
+    
+    for flip in range(0,10):
+        flip_result = np.random.choice([0, 1])
+        flip_results.append(flip_result)
+    
+    number_heads.append(np.sum(flip_results))   
+        
+number_heads
+
+plt.hist(number_heads,bins=np.arange(-0.5,11.5,1.0),density=True)
+plt.xlabel('Number of heads out of 10 attempts, in 10 trials',  fontsize=14)
+plt.show()
+
+Instead of doing 10 coin flips 10 times, do 10 coin flips 1000 times. Plot the histogram of the result.
+
+number_heads = []
+
+for flip_experiment in range (0,1000):
+
+    flip_results = []
+    
+    for flip in range(0,10):
+        flip_result = np.random.choice([0, 1])
+        flip_results.append(flip_result)
+    
+    number_heads.append(np.sum(flip_results))   
+        
+plt.hist(number_heads,bins=np.arange(-0.5,11.5,1.0),density=True)
+plt.xlabel('Number of heads out of 10 attempts, in 1000 trials',  fontsize=14)
+plt.show()
+
+## Basic statistical concepts
+
+_Statistics_ are the methods by which we analyze, interpret and model data. It is helpful to understand a few concepts: 
+
+1) **Population** versus **sample**: the _population_ is the set of all possible outcomes of a given measurement (if you had an infinite number of data points), while the _sample_ is what you have - a finite number of data points.  
+
+2) **Probability**: the measure of how likely it is for a particular event to occur.  If something is impossible, it has a probability $P$ of 0.  If it is a certainty, it has a probability $P$ of 1. In our coin flip experiment both heads and tails had equal probabilty of 0.5.
+
+3) **Theoretical** versus **empirical** distributions: Empirical distributions are measured data. Theoretical distributions are analytical probabililty functions described with an equation.  These can be applied to data, allowing interpretations about the likelihood of observing a given measurement.  
+
+ 
+Additional resources: Olea (2008): https://pubs.usgs.gov/of/2008/1017/ofr2008-1017_rev.pdf; Davis, J. (2002): Statistical and Data Analysis in Geology, 3rd Ed, Wiley, Hoboken. 
+
+There are many different types of probability distributions and evaluating the equations gives us a theoretical distribution. 
+
+Samples are finite collections of observations which belong to a distribution. In this exercise, we will simulate 'measurements' by drawing 'observations' from a theoretical distribution.  This is the _Monte Carlo_ approach (named after the gambling town). 
+
+**We just simulated an experiment of flipping a coin. Let's compare what we got through that simulation to the theoretical distribution.**
+
+## Probability Distributions 
+### Binomial Distribution
+
+A relatively straight-forward distribution is the _binomial_ distribution which describes the probability of a particular outcome when there are only two possibilities (yes or no, heads or tails, 1 or 0).   For example, in a coin toss experiment (heads or tails), if we flip the coin  $n$ times, what is the probability of getting $x$ 'heads'?  We assume that the probability $p$ of a head for any given coin toss is 50%; put another way $p$ = 0.5.  
+
+The binomial distribution can be described by an equation: 
+
+$$P=f(x,p,n)= \frac{n!}{x!(n-x)!}p^x(1-p)^{n-x}$$
+
+We can look at this kind of distribution by evaluating the probability for getting $x$ 'heads' out of $n$ attempts. We'll code the equation as a function, and calculate the probability $P$ of a particular outcome (e.g., $x$ heads in $n$ attempts). 
+
+Note that for a coin toss, $p$ is 0.5, but other yes/no questions can be investigated as well (e.g., chance of finding a fossil in a sedimentary layer, whether or not a landslide occurs following an earthquake). 
+
+
+n=10 # number of attempts in each trial
+p=0.5 # probability of getting a head
+x=np.arange(0,n+1,1) # range of test values from 0,N
+
+# initialize size of probability array
+bi_prob = np.zeros(len(x)) 
+
+# compute the binomial probability of getting x outcomes in n attempts
+for i in np.arange(0,len(x),1):
+    bi_prob[i] = (np.math.factorial(n)*(p**(x[i]))*(1.-p)**(n-x[i]) /(np.math.factorial(x[i])*np.math.factorial(n-x[i])))
+
+#plot the probability distribution
+plt.plot(x,bi_prob,'ro',linewidth=2) # plot as solid line
+plt.xlabel('Number of heads out of 10 attempts, $\infty$ trials',  fontsize=14) # add labels
+plt.ylabel('Probability',  fontsize=14) 
+plt.title('Binomial Distribution')
+plt.show()
+
+Notice how similiar this is to the histogram we made of our 1000 experiments of 10 coin flips. Also notice that with only 10 experiments the empirical histogram of heads does not look much like the theoretical distribution.
+
+### Poisson Distribution
+
+The Poisson distribution gives the probability that an event (with two possible outcomes) occurs $k$ number of times in an interval of time where $\lambda$ is the expected rate of occurance. The Poisson distribution is the limit of the binomial distribution for large $n$. So if you take the limit of the binomial distribution as $n \rightarrow \infty$
+
+$\lim_{n \rightarrow \infty } \frac{n!}{k!(n-k)!}p^k(1-p)^{n-k} = \frac{n!}{k!(n-k)!}(\frac{\lambda}{n})^k(1-\frac{\lambda}{n})^{n-k} $
+
+you'll get the Poisson distribution:
+
+$P(k$ events in interval$) = e^{-\lambda}\frac{\lambda^{k}}{k!}.$
+
+Because $\lim_{x \rightarrow \infty } (1+\frac{a}{x})^{x}=e^{a}$ and $\lim_{x \rightarrow \infty } \frac{x!}{x^{k}(x-k)!} = 1$
+
+**Watach these two videos explaining Poisson processes, from Khan Academy:**
+
+[Poisson Process 1](https://www.khanacademy.org/math/statistics-probability/random-variables-stats-library/poisson-distribution/v/poisson-process-1)
+
+[Poisson Process 2](https://www.khanacademy.org/math/statistics-probability/random-variables-stats-library/poisson-distribution/v/poisson-process-2)
+
+
+
+lam = 4 # average rate of occurance (e.g. cars per hour)
+k = np.arange(0,21,1) # number of times the event actually occurs (e.g. observed cars in an hours)
+
+# initialize size of probability array
+pos_prob = np.zeros(len(k)) 
+
+# compute the binomial probability of getting x outcomes in n attempts
+for i in np.arange(0,len(k),1):
+    pos_prob[i] = (np.exp(-1*lam))*(lam**k[i])/np.math.factorial(k[i])
+    
+# plot    
+plt.plot(k,pos_prob,'ro',label='$\lambda$ = 4') # theoretical curve for lam = 4
+plt.xlabel('$k$, number of events',  fontsize=14)
+plt.ylabel('Probability of $k$ events',  fontsize=14)
+plt.title('Poisson Distribution');
+plt.legend();
+
+### Gamma Distribution
+
+The Gamma distribution gives the probability of a waiting interval between Poisson distributed events (that is an event that randomly occurs but for which is there is an average time period between the events).
+
+Consider the distribution function $D(x)$ of waiting times until the $h$th Poisson event given a Poisson distribution with a rate of occurance $\lambda$,
+
+$$ D(x) = P (X \le x) = 1 - P(X > x) = 1-\sum_{k=0}^{h-1}\frac{(\lambda x)^{k}e^{-\lambda x}}{k!} = 1-e^{-\lambda x}\sum_{k=0}^{h-1}\frac{(\lambda x)^{k}}{k!} = 1-\frac{\Gamma(h,x\lambda) }{\Gamma (h)}$$ 
+
+where $\Gamma (x) = (x-1)!$ is a complete gamma function and $\Gamma (n,x) = (n-1)! e^{-x}\sum_{k=0}^{n-1}\frac{x^{k}}{k!}$ an incomplete gamma function. The corresponding probability function $P(x)$ of waiting times until the $h$th Poisson event is then obtained by differentiating  $D(x)$,
+
+$$ P(x) = D'(x) = \frac{\lambda (\lambda x)^{h-1}}{(h-1)!}e^{-\lambda x} $$
+
+Now let $\alpha=h$ (not necessarily an integer) and define $\theta=1/\lambda$ to be the time between occurances. Then the above equation can be written
+
+$$ P(x) = \frac{x^{\alpha-1}e^{-x/\theta}}{\Gamma (\alpha) \theta^{\alpha}} $$
+
+which is the probability of a duration time $x$ between events.
+
+# theoretical gamma prob distribution
+alpha = 1.2
+avg_dur = 1/4 # average wait time (4 cars per hours so 1/4 hour)
+theta = avg_dur / alpha;
+wait_time = np.concatenate((np.arange(0.0,0.1,0.001),np.arange(0.1,1.0,0.1)), axis=None)
+gamma_prob = np.zeros(len(wait_time))
+for i in np.arange(0,len(wait_time),1):
+    gamma_prob[i] = (wait_time[i]**(alpha - 1) * np.exp(-1*wait_time[i]/theta)) / (scipy.special.gamma(alpha)* theta**alpha)
+
+
+# plot
+plt.plot(wait_time,gamma_prob,'ro') # theoretical curve 
+plt.xlabel('Wait time Duration',  fontsize=14)
+plt.ylabel('Probability of a wait time',  fontsize=14)
+plt.title('Gamma Distribution')
+plt.show()
+
---
+++ _build/jupyter_execute/folder_06/W6_random_earth.py
@@ -0,0 +1,73 @@
+# 6.2 Examples of Random Earth Processes
+
+**Outline:**
+* A few examples of Earth System processes that are random (there are many more)
+
+## Radioactive Isotope Decays
+Elements on the periodic table come in different "flavors" called _isotopes_. All isotopes of a given element will have the same number of protons, but can have different numbers of neutrons. Some isotopes are _unstable_ and will _decay_, meaning that they will go through a process to gain or lose neutrons to become a _stable isotope_. The decay of radioactive isotopes follows a Poisson distribution.
+
+**Watch this video on Radioactive Decay:**
+
+[![](http://img.youtube.com/vi/zXw2cOSBB8E/0.jpg)](http://www.youtube.com/watch?v=zXw2cOSBB8E "")
+
+The _half-life_ of an unstable isotope is the time that it takes for half of the atoms in your sample to decay to the stable form. For example the half-life ($t_{1/2}$) of Carbon-14 is 5730 years (or 180,825,048,000 seconds). The average number of decays/second is called the _activity_, $\lambda=Np$ where $N$ is the number of parent isotope nuclei in our hypothetical samples (i.e. the number of potential decays, let's say $N=1 \times 10^{12}$ atoms) and $p=\frac{\ln⁡2}{t_{1/2}}$ is the probability that an atom will decay. So $\lambda=Np = 1 \times 10^{12} \times \ln2/ 1.8 \times 10^{11} = 3.8 $ decays/second.
+
+The theoretical probability curve of number of decay events ($k$) is $P(k)=\frac{\lambda^{k} e^{-\lambda}}{k!}$, the Poisson!
+
+
+
+## Geomagnetic Reversals 
+Earth has a magnetic field that is generated in the outer core and is dominantly a dipole that is aligned with Earth's spin axis:
+
+![Magnetic field](./figures/magnetic_field.png)
+
+Earth’s magnetic field varies with time. The most dramatic aspect of this variation is that it reverses its polarity. The field structure in both the normal and reversed states is dipolar (like a bar magnetic), but the pole locations are switched. The timescale that it takes for the field to reverse is relatively short (a few thousand years) compared to the time that it is typically in a given polarity. Earth's magnetic reverses due to complex magnetohydrodynamic processes associated with convection of liquid iron within the outer core. These processes can be captured in numerical models such as this one:
+
+![simulated reversal](./figures/simulation_reversal.png)
+
+> Source: Glatzmaier and Coe (2015) DOI:10.1016/b978-0-444-53802-4.00146-9
+
+The Geomagnetic Polarity Time Scale is the record of geomagnetic reversals over the past ~150 million years largely derived from seafloor magnetic anomalies, but also records developed from rocks on land:
+
+![GPTS](./figures/GPTS.png)
+
+> Source: Gee and Kent (2007) "Source of Oceanic Magnetic Anomalies and the Geomagnetic Polarity Timescale"
+
+
+You have now dealt in detail with data that is developed by research vessels towing a magnetometer measure the marine magnetic anomalies. As you saw, the history of reversals is recorded by the oceanic crust as it forms at the ridge with both sides of the ridge record this pattern of reversals leading to symmetry about the ridge. Both the marine magnetic anomalies and records of the magnetic field on land in sedimentary rocks and lava flows have led to the development of the geomagnetic polarity time scale (GPTS). 
+
+Geomagnetic reversals are often interpretted to behave like a Poisson process. A Poisson process meets the following criteria:
+
+- Events are independent of each other.
+- The average rate (events per time period) is constant.
+- Two events cannot occur at the same time.
+
+So the probability of the number of reversal events ($k$) is $$P(k)=\frac{\lambda^{k} e^{-\lambda}}{k!}$$ where $\lambda$ is the average reversal rate which has been $\sim4$ reversals/Myr for the past 10 Ma.
+
+The probability of a _polarity chron_ duration i.e. the amount of time between reversals is then given by the Gamma distribution: $$ P(x) = \frac{x^{\alpha-1}e^{-x/\theta}}{\Gamma (\alpha) \theta^{\alpha}} $$
+
+$\theta$ is the expected time between reversals and we will follow McFadden (1984) and define $\theta = \mu / \alpha$ where $\mu$ is the average chron duration. A value for $\alpha$ greater than one can be interpreted either as an artifact linked to some short intervals missing in the GPTS or to some short term memory within the dynamo that would inhibit a second reversal just after a first one has occurred. McFadden (1984) use a value for $\alpha$ of 1.2.
+
+![alpha greater than one](./figures/alpha_greater_one.png)
+
+
+> Source: McFadden (1984) "Statistical Tools for the Analysis of Geomagnetic Reversal Sequence"
+
+
+## Observing meteors
+The number of meteors (meteoroids, comets, or asteroids that enter Earth's atmosphere and burn up) can be modeled with a Poisson distrubution. Meteors events are indepedent of each other and the rate of meteors is approximately constant for a given shower.
+
+![meteors](./figures/AMS_TERMINOLOGY.png)
+
+
+### From https://www.amsmeteors.org/meteor-showers/meteor-faq/:
+
+> **How big are most meteoroids? How fast do they travel?** The majority of visible meteors are caused by particles ranging in size from about that of a small pebble down to a grain of sand, and generally weigh less than 1-2 grams. Those of asteroid origin can be composed of dense stony or metallic material (the minority) while those of cometary origin (the majority) have low densities and are composed of a “fluffy” conglomerate of material, frequently called a “dustball.” The brilliant flash of light from a meteor is not caused so much by the meteoroid’s mass, but by its high level of kinetic energy as it collides with the atmosphere.
+
+> Meteors enter the atmosphere at speeds ranging from 11 km/sec (25,000 mph), to 72 km/sec (160,000 mph!). When the meteoroid collides with air molecules, its high level of kinetic energy rapidly ionizes and excites a long, thin column of atmospheric atoms along the meteoroid’s path, creating a flash of light visible from the ground below. This column, or meteor trail, is usually less than 1 meter in diameter, but will be tens of kilometers long.
+
+> The wide range in meteoroid speeds is caused partly by the fact that the Earth itself is traveling at about 30 km/sec (67,000 mph) as it revolves around the sun. On the evening side, or trailing edge of the Earth, meteoroids must catch up to the earth’s atmosphere to cause a meteor, and tend to be slow. On the morning side, or leading edge of the earth, meteoroids can collide head-on with the atmosphere and tend to be fast.
+
+> **What is a meteor shower? Does a shower occur “all at once” or over a period of time?** Most meteor showers have their origins with comets. Each time a comet swings by the sun, it produces copious amounts of meteoroid sized particles which will eventually spread out along the entire orbit of the comet to form a meteoroid “stream.” If the Earth’s orbit and the comet’s orbit intersect at some point, then the Earth will pass through this stream for a few days at roughly the same time each year, encountering a meteor shower. The only major shower clearly shown to be non-cometary is the Geminid shower, which share an orbit with the asteroid (3200 Phaethon): one that comes unusually close to the sun as well as passing through the earth’s orbit. Most shower meteoroids appear to be “fluffy”, but the Geminids are much more durable as might be expected from asteroid fragments.
+
+
